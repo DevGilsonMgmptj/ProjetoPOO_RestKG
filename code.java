736e325b131293d3a75d7f819550a4e9e8948a35
Gilson Marcos Paim <82621697+DevGilsonMgmptj@users.noreply.github.com>
2022-05-09 21:27:27 -0300

    Update and rename code to code.java

---
+++ code.java
@@ -0,0 +1,55 @@
+// cardapio
+public class Cardapio{
+    int idproduto;
+    double valorKg;
+    double valorBebida;
+}
+//cartão magnetico
+@ -0,0 +1,26 @@
+public class CartMagnetico{
+
+    boolean status;
+     Static ArrayList  consumo = new ArrayList() 
+     
+    //ideia criar metodo de adição dos produtos consumidos no cartão
+    public boolean sacar(double valor){
+        this.status = CartMagnetico.status
+      boolean retorno = false;
+	   try
+	   {
+		   if (this.status = 1){
+               double conta;
+
+               public void addConsumo (Cardapio valor){
+                   if( consumo == NULL ){  consumo = new ArrayList<consumo>(); }
+                   else ( consumo != NULL){ throw new NullPointerExcption("Cartão Invalido")}
+
+                   this.consumo.add(consumo);
+                   system.out.println("Item adicionado");
+                   }
+                }
+		 }
+    }
+    public List<consumo> getConsumo(){ return consumo;}  
+}
+//consumo
+public class Consumo{
+    int itemTipo;
+    double valorKg;
+    double peso;
+
+
+    //Alimento
+    if(itemTipo == 1){
+         public double valorAlimento()
+
+         double acrescimo = (valorAlimento* 0.1)+5.00; 
+         valorAlimento = (valorKg * peso)+ acrescimo;
+
+    }
+    //bebida
+    else if(itemTipo = 2){
+        //verificar cardapio de bebida e os valores;
+
+    }
+}
